99899008d9b19b95aebf36e7aac8fa9386f1a205
zgp <2088220905@qq.com>
2021-02-26 11:38:04 +0800

    MonoGame-SpiritMove

--- MonoGame-SpiritMove/MonoGame-SpiritMove/Game1.cs
+++ MonoGame-SpiritMove/MonoGame-SpiritMove/Game1.cs
@@ -9,6 +9,9 @@ namespace MonoGame_SpiritMove
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _texture2D;
+        private Vector2 _position;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +31,9 @@ namespace MonoGame_SpiritMove
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            //No Game Box.png Yes Game Box
+            _texture2D = Content.Load<Texture2D>("Game Box");
+            _position = new Vector2(0, 0);
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +43,27 @@ namespace MonoGame_SpiritMove
                 Exit();
 
             // TODO: Add your update logic here
+            //获取图片的运动状态
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                //up
+                _position.Y -= 1;       //_position.Y=_position.Y-1
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.B))
+            {
+                //down
+                _position.Y += 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.C))
+            {
+                //left
+                _position.X -= 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                //right
+                _position.X += 1;
+            }
 
             base.Update(gameTime);
         }
@@ -46,6 +73,9 @@ namespace MonoGame_SpiritMove
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture2D,_position,Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
