88177dde88c08f530fc13af0840a3c61fd187ac3
zgp <2088220905@qq.com>
2021-02-27 10:08:20 +0800

    MonoGame-SpinMove

---
+++ MonoGame-SpinMove/MonoGame-SpinMove/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame_SpinMove
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Sprite _sprite;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            var texture1 = Content.Load<Texture2D>("SpinMove1");
+            _sprite = new Sprite(texture1)
+            {
+                Position = new Vector2(100,200),
+                Origin = new Vector2(texture1.Width,texture1.Height-25),
+            };
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _sprite.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _sprite.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGame-SpinMove/MonoGame-SpinMove/Sprite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame_SpinMove
+{
+    public class Sprite
+    {
+        private Texture2D _texture2D;
+        private float _rotation;
+
+        public Vector2 Position;
+        public Vector2 Origin;
+
+        public float RotationVelocity = 3f;
+        public float LinearVelocity = 4f;
+
+        public Sprite(Texture2D texture2D)
+        {
+            _texture2D = texture2D;
+        }
+
+        public void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                _rotation -= MathHelper.ToRadians(RotationVelocity);
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+                _rotation += MathHelper.ToRadians(RotationVelocity);
+            
+            var direction = new Vector2((float) Math.Cos(MathHelper.ToRadians(90) - _rotation) -
+                                        (float) Math.Sin(MathHelper.ToRadians(90) - _rotation));
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                Position += direction * LinearVelocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture2D,Position,null,Color.Aqua,_rotation,Origin,1,SpriteEffects.None,0f);
+        }
+    }
+}
