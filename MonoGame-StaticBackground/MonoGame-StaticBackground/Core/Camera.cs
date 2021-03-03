ff3d4e761964f1cc66c38d42e69c856a42da68d9
zgp <2088220905@qq.com>
2021-03-03 17:34:18 +0800

    MonoGame-StaticBackground

---
+++ MonoGame-StaticBackground/MonoGame-StaticBackground/Core/Camera.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGame_StaticBackground.Core
+{
+    public class Camera
+    {
+        public Matrix TransForm { get; set; }
+
+        public void Follow(Vector2 vector2)
+        {
+            var position = Matrix.CreateTranslation(vector2.X, vector2.Y, 0);
+
+            var offset = Matrix.CreateTranslation(400, 240, 0);
+
+            TransForm = position * offset;
+        }
+    }
+}
--- MonoGame-StaticBackground/MonoGame-StaticBackground/Game1.cs
+++ MonoGame-StaticBackground/MonoGame-StaticBackground/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame_StaticBackground.Core;
 
 namespace MonoGame_StaticBackground
 {
@@ -8,6 +9,11 @@ namespace MonoGame_StaticBackground
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Camera _camera;
+        private Texture2D _PlayTexture;
+        private Texture2D _backgorundTexture;
+        private Vector2 _playerPosition;
 
         public Game1()
         {
@@ -28,6 +34,9 @@ namespace MonoGame_StaticBackground
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _camera = new Camera();
+            _PlayTexture =Content.Load<Texture2D>("Square");
+            _backgorundTexture =Content.Load<Texture2D>("background");
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,15 +46,44 @@ namespace MonoGame_StaticBackground
                 Exit();
 
             // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _playerPosition.Y -= 3f;
+            }
 
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                _playerPosition.Y += 3f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _playerPosition.X -= 3f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _playerPosition.X += 3f;
+            }
+            
+            _camera.Follow(_playerPosition);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backgorundTexture,new Vector2(0,0),Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin(transformMatrix:_camera.TransForm);
+            //_spriteBatch.Draw(_backgorundTexture,new Vector2(0,0),Color.White);
+            _spriteBatch.Draw(_PlayTexture, _playerPosition, Color.Green);
+            _spriteBatch.Draw(_PlayTexture,new Vector2(0,0),Color.Red);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
