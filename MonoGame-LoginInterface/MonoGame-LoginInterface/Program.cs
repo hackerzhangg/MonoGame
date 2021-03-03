e4f1d82904a3bb461dc69bddf81be9e087a3f272
zgp <2088220905@qq.com>
2021-03-03 17:40:01 +0800

    MonoGame-LoginInterface

---
+++ MonoGame-LoginInterface/MonoGame-LoginInterface/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoGame_LoginInterface
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
