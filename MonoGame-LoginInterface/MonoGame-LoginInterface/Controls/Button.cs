2d050a273568da4a1bc9398f5c48840231280ae4
zgp <2088220905@qq.com>
2021-03-04 11:09:21 +0800

    MonoGame-LoginInterface

---
+++ MonoGame-LoginInterface/MonoGame-LoginInterface/Controls/Button.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame_LoginInterface.Controls
+{
+    public class Button : Component.Component
+    {
+
+        #region Fields
+        private MouseState _currentMouse;
+        private SpriteFont _font;
+        private bool _isHovering;
+        private MouseState _previousMouse;
+        private Texture2D _texture2D;
+        #endregion
+        
+        #region Properties
+
+        public event EventHandler Click;
+        public bool Clicked { get; private set; }
+        public Color PenColour { get; private set; }
+        public Vector2 Position { get; set; }
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, _texture2D.Width, _texture2D.Height);
+            }
+        }
+
+        public string Text;
+        #endregion
+
+        #region Methods
+
+        public Button(Texture2D texture, SpriteFont font)
+        {
+            _texture2D = texture;
+
+            _font = font;
+
+            PenColour = Color.Black;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var colour = Color.White;
+
+            if (_isHovering)
+                colour = Color.Gray;
+
+            spriteBatch.Draw(_texture2D, Rectangle, colour);
+
+            if(!string.IsNullOrEmpty(Text))
+            {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
+
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
+
+            _isHovering = false;
+
+            if(mouseRectangle.Intersects(Rectangle))
+            {
+                _isHovering = true;
+
+                if(_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
+                {
+                    Click?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- MonoGame-LoginInterface/MonoGame-LoginInterface/Game1.cs
+++ MonoGame-LoginInterface/MonoGame-LoginInterface/Game1.cs
@@ -1,53 +1,125 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using MonoGame_LoginInterface.Controls;
 
 namespace MonoGame_LoginInterface
 {
-    public class Game1 : Game
+  /// <summary>
+  /// This is the main type for your game.
+  /// </summary>
+  public class Game1 : Game
+  {
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    private Color _backgroundColour = Color.CornflowerBlue;
+
+    private List<Component.Component> _gameComponents;
+
+    public Game1()
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
-        public Game1()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-        }
-
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
-        }
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
     }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+      IsMouseVisible = true;
+
+      base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+
+      var randomButton = new Button(Content.Load<Texture2D>("Controls/Button1"), Content.Load<SpriteFont>("Fonts/Font"))
+      {
+        Position = new Vector2(350, 200),
+        Text = "Random",
+      };
+
+      randomButton.Click += RandomButton_Click;
+
+      var quitButton = new Button(Content.Load<Texture2D>("Controls/Button1"), Content.Load<SpriteFont>("Fonts/Font"))
+      {
+        Position = new Vector2(350, 250),
+        Text = "Quit",
+      };
+
+      quitButton.Click += QuitButton_Click;
+
+      _gameComponents = new List<Component.Component>()
+      {
+        randomButton,
+        quitButton,
+      };
+    }
+
+    private void QuitButton_Click(object sender, System.EventArgs e)
+    {
+      Exit();
+    }
+
+    private void RandomButton_Click(object sender, System.EventArgs e)
+    {
+      var random = new Random();
+
+      _backgroundColour = new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+      // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+      foreach (var component in _gameComponents)
+        component.Update(gameTime);
+
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(_backgroundColour);
+
+      spriteBatch.Begin();
+
+      foreach (var component in _gameComponents)
+        component.Draw(gameTime, spriteBatch);
+
+      spriteBatch.End();
+
+      base.Draw(gameTime);
+    }
+  }
 }
